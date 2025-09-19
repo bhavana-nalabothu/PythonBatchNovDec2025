e18d61c1b251656124375460730b8b9a418e75d5
bhavana-nalabothu <bhavananalabothu1@gmail.com>
2025-09-19 01:24:04 +0000

    I have added updated files

---
+++ 01_Introduction/c_dynamic_typing.py
@@ -0,0 +1,23 @@
+"""
+  Python is the dynamic programming language
+  2 types
+  1. static Typed Languages
+  -first declare the variables and 
+  -then use them
+   int a,float b
+   a=10
+  2. Dynamic Typed Languges
+  -create when you need.No declaration needed
+  num1=123 
+"""
+num=100
+print(type(num))
+
+num=100.
+print(type(num))
+
+num=100j
+print(type(num))
+
+num=True
+print(type(num))
